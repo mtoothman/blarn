94a7f4823596bfdc6488dd8e18ae0f136f98d7e6
Matt Toothman <matt.toothman@aver.io>
2020-07-21 21:21:02 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Starter Blarn`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Matt Toothman`,
+      summary: `Globally unique identifier.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `notfound`,
     },
   },
   plugins: [
